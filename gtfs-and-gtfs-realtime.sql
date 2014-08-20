f60d41efab3a2f74c76dd52df128d997b113fcbe
Darren Embry <dse@webonastick.com>
2014-08-20 17:26:08 -0400

    add some more stuffs.

--- gtfs-and-gtfs-realtime.sql
+++ gtfs-and-gtfs-realtime.sql
@@ -1,26 +1,22 @@
-drop table if exists geo_gtfs;
-create table geo_gtfs (				name				varchar(32)	not null	primary key,
+create table if not exists geo_gtfs (		name				varchar(32)	not null	primary key,
 						value				text		null
 );
 delete from geo_gtfs where name = 'geo_gtfs.db.version';
 insert into geo_gtfs (name, value) values('geo_gtfs.db.version', '0.1');
 
-drop table if exists geo_gtfs_agency;
-create table geo_gtfs_agency (			id				integer				primary key autoincrement,
+create table if not exists geo_gtfs_agency (	id				integer				primary key autoincrement,
 						name				varchar(64)	not null	-- preferably the transit agency's domain name, without a www. prefix. - examples: 'ridetarc.org', 'ttc.ca'
 );
 create index geo_gtfs_agency_01 on geo_gtfs_agency(name);
 
-drop table if exists geo_gtfs_feed;
-create table geo_gtfs_feed (			id				integer				primary key autoincrement,
+create table if not exists geo_gtfs_feed (	id				integer				primary key autoincrement,
 						geo_gtfs_agency_id		integer		not null	references geo_gtfs_agency(id),
 						url				text		not null,
 						is_active			integer		not null	-- updated when feeds added, removed
 );
 create index geo_gtfs_feed_01 on geo_gtfs_feed(is_active);
 
-drop table if exists geo_gtfs_feed_instance;
-create table geo_gtfs_feed_instance (		id				integer				primary key autoincrement,
+create table if not exists geo_gtfs_feed_instance (		id				integer				primary key autoincrement,
 						geo_gtfs_feed_id		integer		not null	references geo_gtfs_feed(id),
 						filename			text		not null,
 						retrieved			integer		not null,
@@ -29,8 +25,7 @@ create table geo_gtfs_feed_instance (		id				integer				primary key autoincremen
 );
 create index geo_gtfs_feed_instance_01 on geo_gtfs_feed_instance(is_latest);
 
-drop table if exists geo_gtfs_realtime_feed;
-create table geo_gtfs_realtime_feed (		id				integer				primary key autoincrement,
+create table if not exists geo_gtfs_realtime_feed (		id				integer				primary key autoincrement,
 						geo_gtfs_agency_id		integer		not null	references geo_gtfs_agency(id),
 						url				text		not null,
 						feed_type			varchar(16)	not null,	-- 'updates', 'positions', 'alerts', 'all'
@@ -39,18 +34,17 @@ create table geo_gtfs_realtime_feed (		id				integer				primary key autoincremen
 create index geo_gtfs_realtime_feed_01 on geo_gtfs_realtime_feed(feed_type);
 create index geo_gtfs_realtime_feed_02 on geo_gtfs_realtime_feed(is_active);
 
-drop table if exists geo_gtfs_realtime_feed_instance;
-create table geo_gtfs_realtime_feed_instance (	id				integer				primary key autoincrement,
+create table if not exists geo_gtfs_realtime_feed_instance (	id				integer				primary key autoincrement,
 						geo_gtfs_feed_id		integer		not null	references geo_gtfs_realtime_feed(id),
 						filename			text		not null,
 						retrieved			integer		not null,
 						last_modified			integer		null,
+						timestamp			integer		null,
 						is_latest			integer		not null
 );
 create index geo_gtfs_realtime_feed_instance_01 on geo_gtfs_realtime_feed_instance(is_latest);
 -------------------------------------------------------------------------------
-drop table if exists gtfs_agency;
-create table gtfs_agency (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_agency (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						agency_id			text		null,		-- indexed -- for feeds containing only one agency, this can be NULL.
 						agency_name			text		not null,
 						agency_url			text		not null,
@@ -61,8 +55,7 @@ create table gtfs_agency (			geo_gtfs_feed_instance_id	integer		not null	referen
 );
 create unique index gtfs_agency_01 on gtfs_agency(geo_gtfs_feed_instance_id, agency_id);
 
-drop table if exists gtfs_stops;
-create table gtfs_stops (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_stops (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						stop_id				text		not null,	-- indexed --
 						stop_code			text		null,
 						stop_name			text		not null,
@@ -82,8 +75,7 @@ create        index gtfs_stops_03 on gtfs_stops(geo_gtfs_feed_instance_id, locat
 create        index gtfs_stops_04 on gtfs_stops(geo_gtfs_feed_instance_id, parent_station);
 create        index gtfs_stops_05 on gtfs_stops(geo_gtfs_feed_instance_id, wheelchair_boarding);
 
-drop table if exists gtfs_routes;
-create table gtfs_routes (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_routes (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						route_id			text		not null,	-- indexed --
 						agency_id			text		null		references gtfs_agency(id),
 						route_short_name		text		not null,
@@ -99,8 +91,7 @@ create        index gtfs_routes_02 on gtfs_routes (geo_gtfs_feed_instance_id, ag
 create        index gtfs_routes_03 on gtfs_routes (geo_gtfs_feed_instance_id, route_id);
 create        index gtfs_routes_04 on gtfs_routes (geo_gtfs_feed_instance_id, route_type);
 
-drop table if exists gtfs_trips;
-create table gtfs_trips (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_trips (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						route_id			text		not null	references gtfs_routes(id),
 						service_id			text		not null,	-- indexed --
 						trip_id				text		not null,	-- indexed --
@@ -119,8 +110,7 @@ create        index gtfs_trips_04 on gtfs_trips (geo_gtfs_feed_instance_id, dire
 create        index gtfs_trips_05 on gtfs_trips (geo_gtfs_feed_instance_id, block_id);
 create        index gtfs_trips_06 on gtfs_trips (geo_gtfs_feed_instance_id, shape_id);
 
-drop table if exists gtfs_stop_times;
-create table gtfs_stop_times (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_stop_times (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						trip_id				text		not null	references gtfs_trips(id),
 						arrival_time			varchar(8)	not null,
 						departure_time			varchar(8)	not null,
@@ -135,8 +125,7 @@ create unique index gtfs_stop_times_01 on gtfs_stop_times (geo_gtfs_feed_instanc
 create        index gtfs_stop_times_02 on gtfs_stop_times (geo_gtfs_feed_instance_id, trip_id);
 create        index gtfs_stop_times_03 on gtfs_stop_times (geo_gtfs_feed_instance_id, stop_sequence);
 
-drop table if exists gtfs_calendar;
-create table gtfs_calendar (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_calendar (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						service_id			text		not null,	-- indexed --
 						monday				integer		not null,
 						tuesday				integer		not null,
@@ -159,8 +148,7 @@ create        index gtfs_calendar_08 on gtfs_calendar(geo_gtfs_feed_instance_id,
 create        index gtfs_calendar_09 on gtfs_calendar(geo_gtfs_feed_instance_id, start_date);
 create        index gtfs_calendar_10 on gtfs_calendar(geo_gtfs_feed_instance_id, end_date);
 
-drop table if exists gtfs_calendar_dates;
-create table gtfs_calendar_dates (		geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_calendar_dates (		geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						service_id			text		not null,	-- indexed --
 						`date`				varchar(8)	not null,
 						exception_type			integer		not null
@@ -169,8 +157,7 @@ create        index gtfs_calendar_dates_01 on gtfs_calendar_dates(geo_gtfs_feed_
 create        index gtfs_calendar_dates_02 on gtfs_calendar_dates(geo_gtfs_feed_instance_id, `date`);
 create        index gtfs_calendar_dates_03 on gtfs_calendar_dates(geo_gtfs_feed_instance_id, exception_type);
 
-drop table if exists gtfs_fare_attributes;
-create table gtfs_fare_attributes (		geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_fare_attributes (		geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						fare_id				text		not null,	-- indexed --
 						price				numeric		not null,
 						currency_type			text		not null,
@@ -180,8 +167,7 @@ create table gtfs_fare_attributes (		geo_gtfs_feed_instance_id	integer		not null
 );
 create        index gtfs_fare_attributes_01 on gtfs_fare_attributes(geo_gtfs_feed_instance_id, fare_id);
 
-drop table if exists gtfs_fare_rules;
-create table gtfs_fare_rules (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_fare_rules (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						fare_id				text		not null	references gtfs_fare_attributes(fare_id),
 						route_id			text		null		references gtfs_routes(id),
 						origin_id			text		null,		-- indexed --
@@ -194,8 +180,7 @@ create        index gtfs_fare_rules_03 on gtfs_fare_rules(geo_gtfs_feed_instance
 create        index gtfs_fare_rules_04 on gtfs_fare_rules(geo_gtfs_feed_instance_id, destination_id);
 create        index gtfs_fare_rules_05 on gtfs_fare_rules(geo_gtfs_feed_instance_id, contains_id);
 
-drop table if exists gtfs_shapes;
-create table gtfs_shapes (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_shapes (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						shape_id			text		not null,	-- indexed --
 						shape_pt_lat			numeric		not null,
 						shape_pt_lon			numeric		not null,
@@ -205,8 +190,7 @@ create table gtfs_shapes (			geo_gtfs_feed_instance_id	integer		not null	referen
 create        index gtfs_shapes_01 on gtfs_shapes(geo_gtfs_feed_instance_id, shape_id);
 create        index gtfs_shapes_02 on gtfs_shapes(geo_gtfs_feed_instance_id, shape_id, shape_pt_sequence);
 
-drop table if exists gtfs_frequencies;
-create table gtfs_frequencies (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_frequencies (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						trip_id				text		null		references gtfs_trips(id),
 						start_time			varchar(8)	null, --indexed
 						end_time			varchar(8)	null, --indexed
@@ -217,8 +201,7 @@ create        index gtfs_frequencies_01 on gtfs_frequencies(geo_gtfs_feed_instan
 create        index gtfs_frequencies_02 on gtfs_frequencies(geo_gtfs_feed_instance_id, start_time);
 create        index gtfs_frequencies_03 on gtfs_frequencies(geo_gtfs_feed_instance_id, end_time);
 
-drop table if exists gtfs_transfers;
-create table gtfs_transfers (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_transfers (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						from_stop_id			text		not null	references gtfs_stops(id),
 						to_stop_id			text		not null	references gtfs_stops(id),
 						transfer_type			integer		not null,
@@ -227,8 +210,7 @@ create table gtfs_transfers (			geo_gtfs_feed_instance_id	integer		not null	refe
 create        index gtfs_transfers_01 on gtfs_transfers(from_stop_id);
 create        index gtfs_transfers_02 on gtfs_transfers(to_stop_id);
 
-drop table if exists gtfs_feed_info;
-create table gtfs_feed_info (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
+create table if not exists gtfs_feed_info (			geo_gtfs_feed_instance_id	integer		not null	references geo_gtfs_feed(id),
 						feed_publisher_name		text		not null,
 						feed_publisher_url		text		not null,
 						feed_lang			text		not null,
---
+++ gtfs-commands.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+gtfs2 () {
+    run Geo-GTFS2/bin/gtfs2 "$@"
+}
+run () {
+    echo "+++ $@"
+    $@
+}
+
+rm -fr ~/.geo-gtfs2/google_transit.sqlite
+
+gtfs2 ridetarc.org http://googletransit.ridetarc.org/feed/google_transitAUG2014.zip
+gtfs2 ridetarc.org http://googletransit.ridetarc.org/realtime/alerts/Alerts.pb
+gtfs2 ridetarc.org http://googletransit.ridetarc.org/realtime/gtfs-realtime/TrapezeRealTimeFeed.pb
+gtfs2 ridetarc.org http://googletransit.ridetarc.org/realtime/trip_update/TripUpdates.pb
+gtfs2 ridetarc.org http://googletransit.ridetarc.org/realtime/vehicle/VehiclePositions.pb
+gtfs2 ridetarc.org http://googletransit.ridetarc.org/realtime/vehicle/VehiclePositions.pb
+
+cat <<EOF | gtfs2 sqlite
+.headers on
+select * from geo_gtfs_realtime_feed;
+EOF
+
