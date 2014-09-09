813f0d085ffa7d4d009b8dcef81e89427a80f7f3
Darren Embry <dse@webonastick.com>
2014-09-09 16:34:21 -0400

    disable some shit for now. use updated shit in GTFS2.pm.

--- Geo-GTFS2/lib/Geo/GTFS2/CommandLine.pm
+++ Geo-GTFS2/lib/Geo/GTFS2/CommandLine.pm
@@ -15,28 +15,19 @@ sub gtfs2 {
     return $self->{gtfs2} = Geo::GTFS2->new();
 }
 
-sub cmd__update {
-    my ($self, $geo_gtfs_agency_name) = @_;
-    $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->update();
-}
-sub help__update { {
-    required => "AGENCY_NAME"
-} }
-
-sub cmd__update_realtime {
-    my ($self, $geo_gtfs_agency_name) = @_;
-    $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->fetch_all_realtime_feeds();
-}
-sub help__update_realtime { {
-    required => "AGENCY_NAME"
-} }
+# sub cmd__update_realtime {
+#     my ($self, $geo_gtfs_agency_name) = @_;
+#     $self->gtfs2->set_agency($geo_gtfs_agency_name);
+#     $self->gtfs2->fetch_all_realtime_feeds();
+# }
+# sub help__update_realtime { {
+#     required => "AGENCY_NAME"
+# } }
 
 sub cmd__realtime_status {
     my ($self, $geo_gtfs_agency_name) = @_;
     $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->print_realtime_status();
+    $self->gtfs2->realtime_status();
 }
 sub help__realtime_status { {
     required => "AGENCY_NAME"
@@ -45,71 +36,71 @@ sub help__realtime_status { {
 sub cmd__trip_status {
     my ($self, $geo_gtfs_agency_name, $trip_id) = @_;
     $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->print_trip_status($trip_id);
+    $self->gtfs2->trip_status($trip_id);
 }
 sub help__trip_status { {
     required => "AGENCY_NAME TRIP_ID"
 } }
 
-sub cmd__realtime_status_raw {
-    my ($self, $geo_gtfs_agency_name) = @_;
-    $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->print_realtime_status_raw();
-}
-sub help__realtime_status_raw { {
-    required => "AGENCY_NAME"
-} }
+# sub cmd__realtime_status_raw {
+#     my ($self, $geo_gtfs_agency_name) = @_;
+#     $self->gtfs2->set_agency($geo_gtfs_agency_name);
+#     $self->gtfs2->print_realtime_status_raw();
+# }
+# sub help__realtime_status_raw { {
+#     required => "AGENCY_NAME"
+# } }
 
-sub cmd__list_realtime_feeds {
-    my ($self, $geo_gtfs_agency_name) = @_;
-    $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->list_realtime_feeds();
-}
-sub help__list_realtime_feeds { {
-    required => "AGENCY_NAME"
-} }
+# sub cmd__list_realtime_feeds {
+#     my ($self, $geo_gtfs_agency_name) = @_;
+#     $self->gtfs2->set_agency($geo_gtfs_agency_name);
+#     $self->gtfs2->list_realtime_feeds();
+# }
+# sub help__list_realtime_feeds { {
+#     required => "AGENCY_NAME"
+# } }
 
-sub cmd__realtime_summary {
-    my ($self, $geo_gtfs_agency_name) = @_;
-    $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->realtime_status(summary => 1);
-}
-sub help__realtime_summary { {
-    required => "AGENCY_NAME"
-} }
+# sub cmd__realtime_summary {
+#     my ($self, $geo_gtfs_agency_name) = @_;
+#     $self->gtfs2->set_agency($geo_gtfs_agency_name);
+#     $self->gtfs2->realtime_status(summary => 1);
+# }
+# sub help__realtime_summary { {
+#     required => "AGENCY_NAME"
+# } }
 
-sub cmd__realtime_raw {
-    my ($self, $geo_gtfs_agency_name) = @_;
-    $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->realtime_status(raw => 1);
-}
-sub help__realtime_raw { {
-    required => "AGENCY_NAME",
-    optional => "A B C D"
-} }
+# sub cmd__realtime_raw {
+#     my ($self, $geo_gtfs_agency_name) = @_;
+#     $self->gtfs2->set_agency($geo_gtfs_agency_name);
+#     $self->gtfs2->realtime_status(raw => 1);
+# }
+# sub help__realtime_raw { {
+#     required => "AGENCY_NAME",
+#     optional => "A B C D"
+# } }
 
-sub cmd__sqlite {
-    my ($self) = @_;
-    $self->gtfs2->exec_sqlite_utility();
-}
-sub help__sqlite { {
-} }
+# sub cmd__sqlite {
+#     my ($self) = @_;
+#     $self->gtfs2->exec_sqlite_utility();
+# }
+# sub help__sqlite { {
+# } }
 
-sub cmd__list_routes {
-    my ($self, $geo_gtfs_agency_name) = @_;
-    $self->gtfs2->set_agency($geo_gtfs_agency_name);
-    $self->gtfs2->list_routes();
-}
-sub help__list_routes { {
-    required => "AGENCY_NAME"
-} }
+# sub cmd__list_routes {
+#     my ($self, $geo_gtfs_agency_name) = @_;
+#     $self->gtfs2->set_agency($geo_gtfs_agency_name);
+#     $self->gtfs2->list_routes();
+# }
+# sub help__list_routes { {
+#     required => "AGENCY_NAME"
+# } }
 
-sub cmd__list_agencies {
-    my ($self) = @_;
-    $self->gtfs2->list_agencies();
-}
-sub help__list_agencies { {
-} }
+# sub cmd__list_agencies {
+#     my ($self) = @_;
+#     $self->gtfs2->list_agencies();
+# }
+# sub help__list_agencies { {
+# } }
 
 sub cmd__url {
     my ($self, $geo_gtfs_agency_name, $url) = @_;
